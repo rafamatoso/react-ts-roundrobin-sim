c925b8256bf95a2cbd111a8dafc7a6be1fec2d99
Rafael Matoso <rafaelmatosog@gmail.com>
2020-07-23 20:50:29 -0300

    feature/Adicionando as dependências husky e lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const App: React.FC = () => {
   return (
-    <section className="hero is-dark is-fullheight">
+    <section className="hero is-dark is-bold is-fullheight">
       <div className="hero-body">
         <div className="container">
           <h1 className="title">Round Robin Sim</h1>
